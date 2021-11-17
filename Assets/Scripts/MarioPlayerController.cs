9ad176adcef5a5a812f1ecf292a9182b1cf1802a
Aida Chavero <achavero@edu.tecnocampus.cat>
2021-11-17 17:02:31 +0100

    nose

--- Assets/Scripts/MarioPlayerController.cs
+++ Assets/Scripts/MarioPlayerController.cs
@@ -19,14 +19,14 @@ public class MarioPlayerController : MonoBehaviour
     [SerializeField] private KeyCode runKey;
     [SerializeField] private float walkSpeed;
     [SerializeField] private float runSpeed;
+    [SerializeField] private float currentSpeed = 0;
     private float verticalSpeed = 0.0f;
     private bool onGround;
 
     private void Update()
     {
         Vector3 movement = Vector3.zero;
-        
-        //Camera control and camera following Mario
+        //Mario moving based on Camera position
         Vector3 l_forward = camera.transform.forward;
         l_forward.y = 0.0f;
         l_forward.Normalize();
@@ -37,17 +37,20 @@ public class MarioPlayerController : MonoBehaviour
         if (Input.GetKey(backKey)) { movement -= l_forward; }
         if (Input.GetKey(rightKey)) { movement += l_right; }
         if (Input.GetKey(leftKey)) { movement -= l_right; }
-        float currentSpeed = Input.GetKey(runKey) ? runSpeed : walkSpeed; //Si es true = run; Si es false = walk
+        else { currentSpeed = 0; }
+        currentSpeed = Input.GetKey(runKey) ? runSpeed : walkSpeed; //Si es true = run; Si es false = walk
         if(movement.magnitude > 0)
         {
             movement.Normalize();
-            
+            transform.forward = movement;
+            movement *= currentSpeed * Time.deltaTime;
         }
         //Jump
         if (Input.GetKeyDown(jumpKey) && onGround)
         {
-            jump();
+            Jump();
         }
+        if(!onGround) { Fall(); }
         //Mario movement
         verticalSpeed += Physics.gravity.y * Time.deltaTime;
         movement.y += verticalSpeed * Time.deltaTime;
@@ -59,14 +62,30 @@ public class MarioPlayerController : MonoBehaviour
         }
         else
         {
+            if((cf & CollisionFlags.Above) != 0 && verticalSpeed > 0)
+            {
+                verticalSpeed = 0;
+            }
             onGround = false;
         }
         animator.SetBool("OnGround", onGround);
+        animator.SetFloat("Speed", currentSpeed);
+        //Idle, Walk & Run animations
+        /*
+        float animSpeed = movement.x == 0 ? 0.0f : 0.03f;
+        if(Input.GetKey(runKey)) { animSpeed = 0.8f; }
+        animator.SetFloat("Speed", animSpeed);
+        */
     }
 
-    private void jump()
+    private void Jump()
     {
         verticalSpeed = jumpSpeed;
         animator.SetTrigger("Jump1");
+    }
+
+    private void Fall()
+    {
+        animator.SetTrigger("Fall");
     }
 }
